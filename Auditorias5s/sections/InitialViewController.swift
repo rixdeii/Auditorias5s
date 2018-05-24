1ff6cde675aa4356da0475fad9ae3b51cfc285e9
Richie Rojas <rrojas@na-at.com.mx>
2018-05-24 17:42:59 -0500

    primer comentario

---
+++ Auditorias5s/sections/InitialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InitialViewController.swift
+//  Auditorias5s
+//
+//  Created by SpotlightOBP on 24/05/18.
+//  Copyright © 2018 SpotlightOBP. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Auditorias5s/sections/cells/WorkStationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkStationTableViewCell.swift
+//  Auditorias5s
+//
+//  Created by SpotlightOBP on 24/05/18.
+//  Copyright © 2018 SpotlightOBP. All rights reserved.
+//
+
+import UIKit
+
+class WorkStationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
